87664e0e4f703779d12320b22e8582e3292cfffc
Nayanmore21 <nayanmore2103@gmail.com>
2025-02-10 19:40:46 +0530

    added history count

--- controllers/url.js
+++ controllers/url.js
@@ -3,12 +3,12 @@ const shortid = require('shortid');
 const URL = require('../models/url');
 
 async function handleGenerateNewShortUrl(req, res) {
-    const {url}  = req.body;
+    const { url } = req.body;
     if (!url) {
         return res.status(400).json({ error: 'url is required..!' });
     }
     const shortID = shortid();
-    
+
     await URL.create({
         shortId: shortID,
         redirectURL: url,
@@ -17,6 +17,13 @@ async function handleGenerateNewShortUrl(req, res) {
     return res.json({ id: shortID });
 }
 
+async function handleGetAnalytics(req, res) {
+    const shortId = req.params.shortId;
+    const result = await URL.findOne({ shortId });
+    return res.json({ totalClicks: result.visitHistory.length, analytics: result.visitHistory })
+}
+
 module.exports = {
     handleGenerateNewShortUrl,
+    handleGetAnalytics,
 }
